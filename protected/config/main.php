e16398b5c49a025d01e89777f6ae3ee49c675e15
Everton Mendonca <evertonmj@gmail.com>
2015-03-24 20:26:57 -0300

    alteracoes

--- protected/config/main.php
+++ protected/config/main.php
@@ -79,6 +79,15 @@ return array(
 				*/
 			),
 		),
+        'urlManager'=>array(
+            'urlFormat'=>'path',
+            'showScriptName'=>false,
+            'rules'=>array(
+                '<controller:\w+>/<id:\d+>'=>'<controller>/view',
+				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
+				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+            ),
+        ),
 	),
 
 	// application-level parameters that can be accessed
